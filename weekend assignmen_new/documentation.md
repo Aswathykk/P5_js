f74b0a380b9eb6ea8477303bfe206de3b21abbf9
aswathykk321-creator <aswathykk321@gmail.com>
2025-10-14 15:04:42 +0530

    .

--- weekend assignmen_new/documentation.md
+++ weekend assignmen_new/documentation.md
@@ -1,5 +1,16 @@
 //////////////////creating an interactive experuence//////////
 A person tied to the busy corporate life, he lost his face emotions and everything in the process. he is going back to home and on the way he regains his face the colrs and the life he lost.
 
-first the office scene where the peron is sitting infront of laptop and clicking mouse then the scene is changing where he is walking throigh traffic(here 10 print patterns are used as background amd walking sprite is overlayed) then sitting in the bus and background city light is changing in the bcaground with person istting as the overlay
-for each action different functions are created
+first the office scene where the peron is sitting infront of laptop and clicking mouse then the scene is changing where he is walking throigh traffic(here 10 print patterns are used as background amd walking sprite is overlayed). for walking we have to press right arrow keys .then sitting in the train and background city light is changing in the background with person sitting as the overlay. then slowly the background image(city) will change to green color and his familiar places, ike reconnecting with the peaceful life. he will se the forest trees and all. and solwly his face will become bright, his features and emotions will appear(indicating him regaining him self back). so here there are some music and some interactions planned like putting earphones, calls and all.
+//////////////////////
+for each action different functions are created and nested for loops and if conditions are used
+
+/////////////Issues/////////
+-- faced issues relate dto connecting all secenes did for 4, 5 times and some bug fix with ai tools then it worked
+-- for city animation got some code from git hub and trried integrating that, but faced issues while inegrating
+
+///////future////
+- wuld like to add some more story
+- this was created as  prototype , so would like to make it better
+- music and texts need to be added
+
--- weekend assignmen_new/sketch.js
+++ weekend assignmen_new/sketch.js
@@ -1,113 +1,178 @@
-let office = [], light = [], city = [], earphone = [];
-let walking, train;
+let officeImages = [], lightImages = [], cityImages = [], earphoneImages = [], walkingImage,trainImage;
 
-let index = 0;
-let gridActive = false, cityActive = false;
-const gridSize = 200;
-let lastChangeTime = 0;
-
-// ---------------- Walking sprite ----------------
-const WALK = { x: 50, y: 0, frame: 0, speed: 2, animSpeed: 5 };
-
-// ---------------- City animation ----------------
-const CITY = { frame: 0, nextFrame: 1, alpha: 0, fadeSpeed: 5 };
-
+let currentOffice = 0;
+let gridIsActive = false;
+let cityIsActive = false;
+let gridSize = 200;
+let lastGridChange = 0;
 let lightGrid = [];
+let walkX = 50;
+let walkY;
+let walkFrame = 0;
+let walkSpeed = 2;
+let walkAnimSpeed = 5;
+let cityFrame = 0;
+let nextCityFrame = 1;
+let cityAlpha = 0;
+let cityFadeSpeed = 5;
 
 function preload() {
-  office = [loadImage('images/office1.png'), loadImage('images/office2.png')];
-  light = [loadImage('images/light1.png'), loadImage('images/light2.png'), loadImage('images/light3.png')];
-  walking = loadImage('images/walking.png'); // 1 row, 2 cols
-  city = [loadImage('images/city1.png'), loadImage('images/city2.png'), loadImage('images/city3.png')];
-  train = loadImage('images/travel dark.png');
-  earphone = [loadImage('images/earphone1.png'), loadImage('images/earphone2.png'), loadImage('images/earphone3.png'),loadImage('images/earphone5.png')];
-  
+  //office
+  officeImages[0] = loadImage('images/office1.png');
+  officeImages[1] = loadImage('images/office2.png');
+
+  // ight pattern images
+  lightImages[0] = loadImage('images/light1.png');
+  lightImages[1] = loadImage('images/light2.png');
+  lightImages[2] = loadImage('images/light3.png');
+
+  // Walking sprite 
+  walkingImage = loadImage('images/walking.png');
+
+  // City 
+  cityImages[0] = loadImage('images/city1.png');
+  cityImages[1] = loadImage('images/city2.png');
+  cityImages[2] = loadImage('images/city3.png');
+
+  // Train
+  trainImage = loadImage('images/travel dark.png');
+
+  // Earphone
+  earphoneImages[0] = loadImage('images/earphone1.png');
+  earphoneImages[1] = loadImage('images/earphone2.png');
+  earphoneImages[2] = loadImage('images/earphone3.png');
+  earphoneImages[3] = loadImage('images/earphone5.png');
 }
 
 function setup() {
   createCanvas(1000, 1000);
   imageMode(CENTER);
-  WALK.y = height - gridSize / 2; // start bottom left
+
+  walkY = height - gridSize / 2; // Start near bottom of screen
 }
 
 function draw() {
   background(255);
 
-  if (!gridActive) {
-    drawImageCenter(office[index], width, height);
-  } else if (!cityActive) {
-    drawLightGrid();
-    drawWalking();
+  if (!gridIsActive) {
+    showOfficeScene();
+  }
+  else if (!cityIsActive) {
+    showLightGrid();
+    showWalkingPerson();
 
-    if (WALK.x > width + 300) cityActive = true;
-  } else {
-    drawCityAnimation();
+    if (walkX > width + 300) {
+      cityIsActive = true;
+      cityAlpha = 0;
+    }
+  }
+  else {
+    showCityScene();
   }
 }
 
-// ---------------- Mouse ----------------
 function mousePressed() {
   if (mouseButton === RIGHT) {
-    index = (index + 1) % office.length;
-    if (index === 0) {
-      gridActive = true;
-      lastChangeTime = millis();
+    if (!gridIsActive) {
+      currentOffice++;
+      if (currentOffice >= officeImages.length) {
+        gridIsActive = true;
+        currentOffice = 0; 
+        lastGridChange = millis();
+        walkX = 50; 
+        walkY = height - gridSize / 2;
+
+        initializeLightGrid();
+      }
     }
   }
 }
-document.oncontextmenu = () => false;
 
-// ---------------- Helpers ----------------
-function drawImageCenter(img, w, h) {
-  if (img) image(img, width / 2, height / 2, w, h);
+document.oncontextmenu = () => false; // Disable right-click menu
+
+function showOfficeScene() {
+  let img = officeImages[currentOffice];
+  if (img) {
+
+    image(img, width / 2, height / 2, width, height);
+
+  }
 }
 
-// ---------------- Light grid ----------------
-function drawLightGrid() {
-  let cols = ceil(width / gridSize), rows = ceil(height / gridSize);
+function initializeLightGrid() {
+  let cols = ceil(width / gridSize);
+  let rows = ceil(height / gridSize);
+  lightGrid = [];
+  for (let i = 0; i < cols; i++) {
+    lightGrid[i] = [];
+    for (let j = 0; j < rows; j++) {
+      lightGrid[i][j] = floor(random(lightImages.length));
+    }
+  }
+}
 
-  if (millis() - lastChangeTime > 2000) {
-    lastChangeTime = millis();
+function showLightGrid() {
+  let cols = ceil(width / gridSize);
+  let rows = ceil(height / gridSize);
+
+  if (millis() - lastGridChange > 2000) {  // eevry 2s
+    lastGridChange = millis();
     for (let i = 0; i < cols; i++) {
-      lightGrid[i] = [];
-      for (let j = 0; j < rows; j++) lightGrid[i][j] = floor(random(light.length));
+      for (let j = 0; j < rows; j++) {
+        lightGrid[i][j] = floor(random(lightImages.length));
+      }
     }
   }
 
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      let imgIdx = lightGrid[i]?.[j] ?? 0;
-      image(light[imgIdx], i * gridSize + gridSize / 2, j * gridSize + gridSize / 2, gridSize, gridSize);
+      let index = lightGrid[i][j];
+      if (index !== undefined) {
+        image(
+          lightImages[index],
+          i * gridSize + gridSize / 2,
+          j * gridSize + gridSize / 2,
+          gridSize,
+          gridSize
+        );
+      }
     }
   }
 }
 
-// ---------------- Walking sprite ----------------
-function drawWalking() {
+function showWalkingPerson() {
+
   if (keyIsDown(RIGHT_ARROW)) {
-    WALK.x += WALK.speed;
-    if (frameCount % WALK.animSpeed === 0) WALK.frame = (WALK.frame + 1) % 2;
-  } else WALK.frame = 0;
-
-  let sx = WALK.frame * walking.width / 2;
-  let sy = 0;
-  image(walking.get(sx, sy, walking.width / 2, walking.height), WALK.x, WALK.y, 600, 600);
-}
-
-// ---------------- City animation ----------------
-function drawCityAnimation() {
-  drawImageCenter(city[CITY.frame], width, height);
-  push();
-  tint(255, CITY.alpha);
-  drawImageCenter(city[CITY.nextFrame], width, height);
-  pop();
-
-  CITY.alpha += CITY.fadeSpeed;
-  if (CITY.alpha >= 255) {
-    CITY.frame = CITY.nextFrame;
-    CITY.nextFrame = (CITY.nextFrame + 1) % city.length;
-    CITY.alpha = 0;
+    walkX += walkSpeed;
+    if (frameCount % walkAnimSpeed === 0) {
+      walkFrame = (walkFrame + 1) % 2;
+    }
+  } else {
+    walkFrame = 0;
   }
 
-  drawImageCenter(train, width, height); // overlay train
+  let frameWidth = walkingImage.width / 2;
+  let sx = walkFrame * frameWidth;
+
+  let singleFrame = walkingImage.get(sx, 0, frameWidth, walkingImage.height);
+  image(singleFrame, walkX, walkY, 600, 600);
+}
+
+function showCityScene() {
+ 
+  image(cityImages[cityFrame], width / 2, height / 2, width, height);
+
+  push();
+  tint(255, cityAlpha);
+  image(cityImages[nextCityFrame], width / 2, height / 2, width, height);
+  pop();
+
+  cityAlpha += cityFadeSpeed;
+  if (cityAlpha >= 255) {
+    cityFrame = nextCityFrame;
+    nextCityFrame = (nextCityFrame + 1) % cityImages.length;
+    cityAlpha = 0;
+  }
+
+  image(trainImage, width / 2, height / 2, width, height);
 }
